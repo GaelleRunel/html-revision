a9cebc1547465ae760e7ed55abdfcbf2d0c2cfac
GaelleRunel <gaelle.runel@gmail.com>
2018-10-15 16:47:00 -0700

    dynamic text js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,4 +7,29 @@ $(function(){
         $(this).addClass("active"); // on l'ajoute à l'item cliqué   
     });
 
+
+    /////////////////////////////////////////////////////////////////////////////////////////////
+
+    // effet d'apparaition du texte Gaelle RUNEL lettre par lettre
+    var name = $("#texte").text().trim();  // trim enlève les espaces au début et à la fin de la chaine
+    console.log (name);
+
+    // on vide la div qui contient le mot (pour éviter qu'il apparaisse 2 fois)
+    $("#texte").empty();
+
+    // parcourir les lettres du mot
+    for(let i=0; i<name.length; i++){
+
+        setTimeout (function(){
+            console.log(name[i]);
+            $("#texte").append(name[i]);
+        },300 * i);                     // 300 = durée de déclenchement --- multipliée par i pour avoir 1*300, 2*300, 3*300,...
+}                                       // ce qui permet d'échelonner l'affichage du texte.
+
 });
+
+
+
+
+ 
+
